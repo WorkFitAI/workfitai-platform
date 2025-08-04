a6627cc8dd63d4798c42d03725141d1e3e644056
lightningflashh <thanhchi0429@gmail.com>
2025-08-04 14:35:18 +0700

    WORKFITAI-20 Config Spring Cloud Bus

--- services/config-server/src/main/java/org/workfitai/configserver/ConfigServerApplication.java
+++ services/config-server/src/main/java/org/workfitai/configserver/ConfigServerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
         org.springframework.cloud.vault.config.VaultAutoConfiguration.class
 })
 @EnableConfigServer
-
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
