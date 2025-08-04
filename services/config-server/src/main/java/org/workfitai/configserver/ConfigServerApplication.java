ee620ed5ea82b4846c49c7a2f68a0feeb114d135
lightningflashh <thanhchi0429@gmail.com>
2025-08-04 10:17:51 +0700

    WORKFITAI-19 Set up Spring Cloud Config Server & Integrate HashiCorp Vault

---
+++ services/config-server/src/main/java/org/workfitai/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package org.workfitai.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication(exclude = {
+        org.springframework.cloud.vault.config.VaultAutoConfiguration.class
+})
+@EnableConfigServer
+
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
