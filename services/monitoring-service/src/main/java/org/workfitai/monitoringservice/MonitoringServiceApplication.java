06328b5a04bdd3a1b64ea9ce99c579ac8d943fbb
Killig3110 <killig.3110@gmail.com>
2025-12-15 00:13:21 +0700

    refactor: Remove unused MongoDB initialization scripts and Vault initialization script; update application configurations to streamline service integration

--- services/monitoring-service/src/main/java/org/workfitai/monitoringservice/MonitoringServiceApplication.java
+++ services/monitoring-service/src/main/java/org/workfitai/monitoringservice/MonitoringServiceApplication.java
@@ -3,11 +3,9 @@ package org.workfitai.monitoringservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableConfigServer
 public class MonitoringServiceApplication {
 
     public static void main(String[] args) {
