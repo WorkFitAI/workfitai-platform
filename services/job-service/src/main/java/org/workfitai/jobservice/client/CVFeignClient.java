b98f9ac9a6b6042427dfc745b86227a58af36280
Killig3110 <killig.3110@gmail.com>
2025-12-20 16:53:50 +0700

    feat: Add Feign configuration for JWT authentication and request interception

--- services/job-service/src/main/java/org/workfitai/jobservice/client/CVFeignClient.java
+++ services/job-service/src/main/java/org/workfitai/jobservice/client/CVFeignClient.java
@@ -4,10 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.workfitai.jobservice.config.FeignConfig;
 
 import java.util.Map;
 
-@FeignClient(name = "cv-service", url = "${service.cv.url:http://localhost:9083}")
+@FeignClient(name = "cv-service", url = "${service.cv.url:http://localhost:9083}", configuration = FeignConfig.class)
 public interface CVFeignClient {
 
     @GetMapping("/candidate/{username}")
